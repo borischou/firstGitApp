5c1ae127f1b1fa5b5097fee36f6eea266fbeeec7
Bo Li Zhou <bolizhou@Boris-MacBook-Pro.local>
2014-06-26 17:26:12 +0800

    first commit

---
+++ OutOftheWorld/OWOutOfWorldTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  OWOutOfWorldTableViewController.h
+//  OutOftheWorld
+//
+//  Created by Bo Li Zhou on 6/25/14.
+//  Copyright (c) 2014 IBM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OWOutOfWorldTableViewController : UITableViewController
+
+@property (strong, nonatomic) NSMutableArray *stories;
+
+@end
